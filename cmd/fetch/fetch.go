32b32df6929944154c7d1d64a79f4e506d29cd34
Wonwoo Seo <wonwooseo@hotmail.com>
2024-03-23 01:03:35 +0900

    sample fetch data

--- cmd/fetch/fetch.go
+++ cmd/fetch/fetch.go
@@ -1,8 +1,15 @@
 package fetch
 
 import (
+	"context"
+	"time"
+
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/wonwooseo/panawa/pkg/price"
+	"github.com/wonwooseo/panawa/pkg/price/kamis"
 )
 
 func Command(baseLogger zerolog.Logger) *cobra.Command {
@@ -13,7 +20,20 @@ func Command(baseLogger zerolog.Logger) *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			logger := baseLogger.With().Str("caller", "cmd/fetch").Logger()
 			logger.Info().Msg("not implemented")
-			// TODO: get data
+
+			apiURL := viper.GetString("api.kamis.url")
+			var priceFetcher price.DataClient = kamis.NewDataClient(baseLogger, apiURL)
+
+			datePrice, regionalMarketPrices, err := priceFetcher.GetDatePrices(context.Background(), time.Date(2024, 3, 20, 6, 0, 0, 0, time.UTC), "0000")
+			if err != nil {
+				logger.Error().Err(err).Msg("failed to fetch price data")
+				return
+			}
+
+			logger.Info().Any("date_price", datePrice).Send()
+			for region, marketPrices := range regionalMarketPrices {
+				logger.Info().Str("region_code", region).Any("market_prices", marketPrices).Send()
+			}
 		},
 	}
 }
